0f395bc78fd7d95c7624a60081b4853f7035d60a
John Philip A. Encarnacion <johphilencarnacion@gmail.com>
2021-01-30 12:53:29 +0800

    Delete Export directory

--- Export/assets/js/barber.js
+++
@@ -1,9 +0,0 @@
-var text_max = 200;
-$('#count_message').html('0 / ' + text_max );
-
-$('#textIntro').keyup(function() {
-  var text_length = $('#textIntro').val().length;
-  var text_remaining = text_max - text_length;
-  
-  $('#count_message').html(text_length + ' / ' + text_max);
-});
